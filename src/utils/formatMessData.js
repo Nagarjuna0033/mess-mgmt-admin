62a7b579fe8c704cb3de7d2470bfbd3d34e32169
revanthkumarJ <jrevanth101@gmail.com>
2024-12-14 11:33:20 +0530

    merged

--- src/utils/formatMessData.js
+++ src/utils/formatMessData.js
@@ -1,18 +1,19 @@
-export const FormatMessData = (res) => {
-    let formattedData = {}; // Use an object to store data by days
-    const data = res.data.data;
-
-    for (let day in data) {
-        let meals = data[day];
-        let formattedMeals = {};
-
-        for (let meal in meals) {
-            const val = meals[meal].reduce((acc, val) => acc + ", " + val.itemName, "");
-            formattedMeals[meal] = val.substring(2); // Remove leading ", "
-        }
-
-        formattedData[day] = formattedMeals; // Assign meals to their respective day
+export const formatMessData = (res) => {
+  let formattedData = [];
+  const data = res.data.data;
+  for (let day in data) {
+    let meals = data[day];
+    let formattedMeals = {};
+    for (let meal in meals) {
+      const val = meals[meal].reduce(
+        (acc, val) => acc + " , " + val.itemName,
+        ""
+      );
+      formattedMeals[meal] = val.substring(2);
     }
 
-    return formattedData; // Return as an object
+    formattedData[day] = formattedMeals;
+  }
+
+  return formattedData;
 };

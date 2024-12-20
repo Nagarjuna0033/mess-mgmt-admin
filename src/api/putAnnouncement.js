967053a4356075f685b156c175c6c7b8fdb676b6
Nagarjuna <nagarjunabanda4@gmail.com>
2024-12-20 22:45:56 +0530

    Removed unused import

--- src/api/putAnnouncement.js
+++ src/api/putAnnouncement.js
@@ -1,13 +1,12 @@
 import { db } from "../firebaseUtils/firebaseConfig";
-import { collection,addDoc } from "firebase/firestore";
+import { collection } from "firebase/firestore";
 
 
 export const addAnnouncement = async (data) => {
     try {
       
       const docRef = await addDoc(collection(db, "Notification"), data);
-  
-    //   console.log("Announcement added successfully with ID:", docRef.id);
+        
       return { success: true, message: "Announcement added successfully" };
     } catch (error) {
       console.error("Error adding announcement:", error);

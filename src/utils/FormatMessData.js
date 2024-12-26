ec235e54607cd1de2e281a8ead6ade115fd97fd1
revanthkumarJ <jrevanth101@gmail.com>
2024-12-26 17:59:01 +0530

    updated

---
+++ src/utils/FormatMessData.js
@@ -0,0 +1,17 @@
+export const formatMessData = (res) => {
+    let FormattedData = {};
+    const data = res.data.data;
+    for (let day in data) {
+      let meals = data[day];
+      let formattedMeals = {};
+      for (let meal in meals) {
+        const val = meals[meal].reduce(
+          (acc, val) => acc + ", " + val.itemName,
+          ""
+        );
+        formattedMeals[meal] = val.substring(2);
+      }
+      FormattedData[day] = formattedMeals;
+    }
+    return FormattedData;
+  };
